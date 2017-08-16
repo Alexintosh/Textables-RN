e899a6944902720138643ef29b29bf4ef0d53112
Alessio <alessio.d@gmail.com>
2017-08-16 15:16:29 +0200

    So it begins...

---
+++ App/Navigation/AppNavigation.js
@@ -0,0 +1,27 @@
+import { StackNavigator } from 'react-navigation'
+import TextablesScreen from '../Containers/TextablesScreen'
+import LaunchScreen from '../Containers/LaunchScreen'
+import LoginScreen from '../Containers/LoginScreen'
+
+import styles from './Styles/NavigationStyles'
+
+// Manifest of possible screens
+const PrimaryNav = StackNavigator({
+  TextablesScreen: { screen: TextablesScreen },
+  LaunchScreen: { screen: TextablesScreen },
+  LoginScreen: {
+    screen: LoginScreen,
+    navigationOptions: { title: 'Login' }
+  }
+}, {
+  // Default config for all screens
+  headerMode: 'none',
+  initialRouteName: 'LaunchScreen',
+  navigationOptions: {
+    header: {
+      style: styles.header
+    }
+  }
+})
+
+export default PrimaryNav
